d3e4f9917049202afaaf9c91b476d50b59673bfa
Hector <alu0101365107@ull.edu.es>
2022-04-04 15:03:01 +0100

    Media resolucion del ej2 y resolucionc completa del ej3

--- Tarea5-Introduccion_a_JavaScript/js/ej2.js
+++ Tarea5-Introduccion_a_JavaScript/js/ej2.js
@@ -1,5 +1,26 @@
-function insertTextInCard() {
+function sub(){
+  titulo = document.getElementsByName("titulo")[0].value;
+  descripcion = document.getElementsByName("descripcion")[0].value;
   var card = document.getElementById("card");
-  var text = document.createTextNode("Añadimos este texto de prueba");
-  card.appendChild(text);
-}
+  const carta = document.createElement('div');
+  carta.classList.add('card', 'blue-grey', 'darken-1', 'card-content', 'white-text', 'card-title');
+  carta.textContent = "" + titulo + descripcion + "";
+  card.appendChild(carta)
+};
+
+
+  // <div class="row">
+  //   <div class="col s12 m6">
+  //     <div class="card blue-grey darken-1">
+  //       <div class="card-content white-text">
+  //         <span class="card-title">Card Title</span>
+  //         <p>I am a very simple card. I am good at containing small bits of information.
+  //         I am convenient because I require little markup to use effectively.</p>
+  //       </div>
+  //       <div class="card-action">
+  //         <a href="#">This is a link</a>
+  //         <a href="#">This is a link</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
--- Tarea5-Introduccion_a_JavaScript/js/ej3.js
+++ Tarea5-Introduccion_a_JavaScript/js/ej3.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
-  alert('Pinchamos');
-  console.log('Pinchamos');
-  $("p").after("<h3> Total div elements: " );
-  return document.getElementsByName('p').length();
-});
+window.onload = function () {
+  var parrafos=document.getElementsByTagName("p");
+  var elementosLista=document.getElementsByTagName("li");
+  var div=document.getElementsByTagName("div");
+  alert("Numero total de parrafos "+parrafos.length + 
+  "\nNumero total de elementos de lista "+elementosLista.length + 
+  "\nNumero total de div "+div.length);
+}
+
