e1ca120e7e20237158860429252027f0c297e948
Hector <alu0101365107@ull.edu.es>
2022-04-04 22:41:12 +0100

    Finalizacion Practica 5

--- Tarea5-Introduccion_a_JavaScript/js/ej2.js
+++ Tarea5-Introduccion_a_JavaScript/js/ej2.js
@@ -1,25 +1,11 @@
+/**
+ * Inserta en una tarjeta un texto que el usuario introduce por teclado
+ */
 function sub(){
   texto = document.getElementsByName("titulo")[0].value;
   var card = document.getElementById("card");
   const carta = document.createElement('div');
-  carta.classList.add('card', 'blue-grey', 'darken-1', 'card-content', 'white-text', 'card-title');
+  carta.classList.add('card', 'white', 'darken-1', 'card-content');
   carta.textContent = "" + texto + "";
   card.appendChild(carta)
 };
-
-
-  // <div class="row">
-  //   <div class="col s12 m6">
-  //     <div class="card blue-grey darken-1">
-  //       <div class="card-content white-text">
-  //         <span class="card-title">Card Title</span>
-  //         <p>I am a very simple card. I am good at containing small bits of information.
-  //         I am convenient because I require little markup to use effectively.</p>
-  //       </div>
-  //       <div class="card-action">
-  //         <a href="#">This is a link</a>
-  //         <a href="#">This is a link</a>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
--- Tarea5-Introduccion_a_JavaScript/js/ej4.js
+++ Tarea5-Introduccion_a_JavaScript/js/ej4.js
@@ -1,4 +1,14 @@
-var BaseDeDatos = {
+/**
+ * Base de datos que registra para cada usuario su:
+ * - DNI
+ * - Año de nacimiento
+ * - Número de cuenta
+ * - Productos de la compra actual
+ * - Descuento (si lo tiene)
+ * - Método de pago
+ * 
+ */
+var dataBase = {
   user1: {
     dni: '43461148B',
     year: 2001,
@@ -20,7 +30,7 @@ var BaseDeDatos = {
       {name: 'muchitos', price: 1.75}
     ],
     discount: 10,
-    paymentMethod: 'credit card'
+    paymentMethod: 'Credit'
   },
   user3: {
     dni: '21940598J',
@@ -32,13 +42,84 @@ var BaseDeDatos = {
       {name: 'agua', price: 1.00},
       {name: 'pizza', price: 5.00}
     ],
-    paymentMethod: 'bitcoin'
+    paymentMethod: 'Bitcoin'
   }
 }
 
 
-i = 1
-console.log(BaseDeDatos["user"+i].products[0].name)
-console.log(BaseDeDatos["user"+i].products[0].price)
-i = 2
-console.log(BaseDeDatos["user"+i].year)
+/**
+ * Calcula el coste de una compra de un usuario concreto.
+ * @user Usuario a elegir
+ * @returns Coste de la compra del usuario elegido
+ */
+function purchaseCostOf(user) {
+  var user = "user" + user;
+  var cost = 0;
+  dataBase[user].products.forEach(element => {
+    cost += element.price;
+  });
+  if (dataBase[user].discount) {
+    cost = cost - (cost*dataBase[user].discount/100)
+  }
+  return cost;
+};
+
+/**
+ * Devuelve el valor de una fecha incrementado en un número de dias
+ * introducido por parámetro.
+ * @days Número de dias a incrementar.
+ */
+function incrementDate(days) {
+  var today = new Date();
+  var day = today.getDate();
+  var month = today.getMonth() + 1;
+  var year = today.getFullYear();
+  if ((today.getDate() + days) > 31) {
+    day = (today.getDate() + days) % 31 + 1;
+    month = today.getMonth() + 2;
+  }
+  if (month > 12) {
+    month = month % 12 + 1;
+    year += 1;
+  }
+  var date = year+'-'+month+'-'+day;
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
+  return dateTime;
+}
+
+/**
+ * Crea una tarjeta en la página que muestra el coste (introducido por
+ * parámetro) de la compra de un usuario específico.
+ * @cost Coste de la compra a mostrar.
+ */
+function showImport(cost, user) {
+  if (dataBase["user" + user].paymentMethod == "Credit") {
+    dateTime = incrementDate(30)
+  } else {
+    dateTime = incrementDate(0)
+  }
+  var card = document.getElementById("precio");
+  const price = document.createElement('div');
+  price.classList.add('card', 'white', 'darken-1', 'card-content');
+  price.textContent = "Precio total: " + cost.toFixed(2);
+  const payDay = document.createElement('div');
+  payDay.classList.add('card', 'white', 'darken-1', 'card-content');
+  payDay.textContent = "Fecha de cobro: " + dateTime;
+  if (!card.firstElementChild && !card.lastElementChild) {
+    card.appendChild(price);
+    card.appendChild(payDay);
+  } else {
+    card.replaceChild(price, card.firstElementChild);
+    card.replaceChild(payDay, card.lastElementChild);
+  }
+}
+
+
+/**
+ * PREGUNTA: El json debe estar en el propio código, inicializando una variable, explica por qué se te exige este requisito.
+ * Las bases de datos deben estar almacenadas en un archivo .json de un servidor, al que se accede desde el
+ * cliente realizando una petición. El cliente por tanto no es el responsable de almacenar información.
+ * Como nosotros no contamos con un servidor, nos vemos obligados a emplear una variable en el código de nuestro
+ * script para almacenar dicha información.
+ */
