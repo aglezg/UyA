6b288dd41cd2909eff6a93d2a96ae797b96c201c
Hector <alu0101365107@ull.edu.es>
2022-04-14 18:44:52 +0100

    Adicion responsive

--- Tarea5-Introduccion_a_JavaScript/js/ej2.js
+++ Tarea5-Introduccion_a_JavaScript/js/ej2.js
@@ -6,7 +6,6 @@ function sub(){
   texto = document.getElementsByName("titulo")[0].value;
   var card = document.getElementById("card");
   const carta = document.createElement('div');
-  carta.classList.add('card', 'white', 'darken-1', 'card-content');
-  carta.textContent = "" + texto + "";
+  carta.innerHTML = "  <div class='row'><div class='col s12 m7'><div class='card'><div class='card-image'><img src='./images/icono.jpg'><span class='card-title'></span></div><div class='card-content'><p>" + texto + "</p></div></div></div></div>"
   card.appendChild(carta)
 };
