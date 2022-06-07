ed77e562c3af55b492e0780424c25d0fc9b3f440
Adrián González Galván <alu0101321219@ull.edu.es>
2022-06-07 21:48:02 +0100

    Actualizamos la cabecera de todas las páginas

--- Proyecto_Final/js/mobileCollapseButton.js
+++ Proyecto_Final/js/mobileCollapseButton.js
@@ -3,4 +3,13 @@ M.AutoInit();
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
-        
+
+$(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+      },
+    });
+});
